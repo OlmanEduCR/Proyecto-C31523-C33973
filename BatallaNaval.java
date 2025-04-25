4c276cb839b78ada75c67be4ee66b611294a3a4b
Olman Eduardo Camacho Jerez <C31523@ecci.ucr.ac.cr>
2025-04-25 17:29:00 -0600

    Primer avance BatallaNaval.java, libre de correcciones

---
+++ BatallaNaval.java
@@ -0,0 +1,53 @@
+public class BatallaNaval{
+    //Atributos
+    private int barcoGrande;
+    private int barcoMediano;
+    private int barcoPequeño;
+    private String jugadorBatallaNaval1;
+    private String jugadorBatallaNaval2;
+    
+    //Setters 
+    public void setBarcoGrande(int barcoGrandeP){
+        this.barcoGrande = barcoGrandeP;
+    }
+
+    public void setBarcoMediano(int barcoMedianoP){
+        this.barcoMediano = barcoMedianoP;
+    }
+
+    public void setBarcoPequeño(int barcoPequeñoP){
+        this.barcoPequeño = barcoPequeñoP;
+    }
+
+    public void setJugadorBatallaNaval1(String jugadorBatallaNaval1P){
+        this.jugadorBatallaNaval1 = jugadorBatallaNaval1P;
+    }
+
+    public void setJugadorBatallaNaval2(String jugadorBatallaNaval2P){
+        this.jugadorBatallaNaval2 = jugadorBatallaNaval2P;
+    }
+
+    //Getters
+    public int getBarcoGrande(){
+        return this.barcoGrande;
+    }
+
+    public int getBarcoMediano(){
+        return this.barcoMediano;
+    }
+
+    public int getBarcoPequeño(){
+        return this.barcoPequeño;
+    }
+
+    public String getJugadorBatallaNaval1(){
+        return this.jugadorBatallaNaval1;
+    }
+
+    public String getJugadorBatallaNaval2(){
+        return this.jugadorBatallaNaval2;
+    }
+
+    //Métodos
+    
+}
