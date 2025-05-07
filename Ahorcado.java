e312ad168a9fbc038d41ac53c68fc40e3868617c
Cristhian892 <crishc1230@gmail.com>
2025-05-06 22:58:06 -0600

    clase ahorcado

--- Ahorcado.java
+++ Ahorcado.java
@@ -1,11 +1,9 @@
-
 public class Ahorcado{
-
-
     //atributo
-
     private String palabraJ1;
     private String palabraJ2;
+    private char[] palabraArrayJ1;
+    private char[] palabraArrayJ2;
     private char[] letrasPalabraJ1;
     private char[] letrasPalabraJ2;
     private boolean[] verificarPJ1;
@@ -20,19 +18,22 @@ public class Ahorcado{
     public Ahorcado(String palabra1, String palabra2) {
     this.palabraJ1 = palabra1.toLowerCase();//para evitar problemas, esro transfa todo la palabra en minuscula ya que puede generar conflicto cuando se compare las entradas del Arrays 
     this.palabraJ2 = palabra2.toLowerCase();
-    
+    this.palabraArrayJ1 = palabra1.toLowerCase().toCharArray();
+    this.palabraArrayJ2 = palabra2.toLowerCase().toCharArray();
     this.letrasPalabraJ1 = new char[palabra1.length()];
     this.letrasPalabraJ2 = new char[palabra2.length()];
-    Arrays.fill(letrasPalabraJ1, '*'); //fill asigna * todas las entradas del arrays 
-    Arrays.fill(letrasPalabraJ2, '*');
-    this.letrasPalabraJ1 = new char[6];
-    this.letrasPalabraJ2 = new char [6];
+    //llenar el array con el signo * para empezar
+    for(int i = 0; i < letrasPalabraJ1.length; i++ ){
+        letrasPalabraJ1[i] = '*';
+    }
+    for(int i=0 ; i < letrasPalabraJ2.length; i++){
+        letrasPalabraJ2[i] = '*';
+    }
     this.verificarPJ1 = new boolean[palabra1.length()];
     this.verificarPJ2 = new boolean[palabra2.length()];
-    Arrays.fill(verificarPJ1, false);
-    Arrays.fill(verificarPJ2, false); //fill asigna falso al array
-    this.intentos1 = 0;
-    this.intentos2 = 0;
+    //llenar el array con valores en falso para empezar a verificar la palabra
+    this.intentos1 = 6;
+    this.intentos2 = 6;
     this.turno = 1;
     this.finDelJuego = false;
     }
@@ -43,6 +44,13 @@ public class Ahorcado{
     }
     public String getPalabraJ2(){
         return palabraJ2;
+    }
+
+    public char getPalabraArrayJ1(){
+        return palabraArrayJ1;
+    }
+    public char getPalabraArrayJ2(){
+        return palabraArrayJ2;
     }
 
     public char getletrasPalabraJ1(){
@@ -78,12 +86,20 @@ public class Ahorcado{
     }
 
     //Setters
-    public String setPalabra(String palabraJ1) {
+    public String setPalabra1(String palabraJ1) {
         this.palabraJ1 = palabraJ1.toLowerCase(); //toLowerCase pasa las mayusculas a minisculas y minisculas no les hace nada
     }
 
-    public String setPalabra(String palabraJ2) {
+    public String setPalabra2(String palabraJ2) {
         this.palabraJ2 = palabraJ2.toLowerCase(); //toLowerCase pasa las mayusculas a minisculas y minisculas no les hace nada
+    }
+
+    public char setPalabraArrayJ1(char palabraArrayJ1) {
+        this.palabraArrayJ1 = palabraArrayJ1;    
+    }
+
+    public char setPalabraArrayJ2(char palabraArrayJ2) {
+        this.palabraArrayJ2 = palabraArrayJ2;    
     }
 
     public char setLetrasPalabraJ1(char[] letrasPalabraJ1) {
@@ -119,23 +135,49 @@ public class Ahorcado{
     }
 
     //metodo intento 
-    public boolean intentosLetraJ1(char letra) {
-        letra = caracter.toLowerCase(letra);//la letra que se ingresa se pone en minuscula sea que se ingrese en mayuscula o minuscula
-        boolean letraEntrada = false;
-        for(){ //ocupamos que devuelva el valor en la posicion i del array palabraJ1 que esta dentro de la cadena 
-            if(int i = 0; i<palabraJ1.length(); i++) {//letra 
-                letrasPalabraJ1[i] = letra; //
-                verificarPJ1 = true;
-                letraEntrada = true;
 
+    public boolean verificarLetraPalabraJ1(char letra) {
+        letra = Character.toLowerCase(letras);
+        if(!Character.isLetter(letra)){
+            return "Ingrese una letra valida";
+        }
+        boolean letraEncontrada = false;
+        for(int i = 0; i< palabraJ1.length(); i++){
+            if (!verificarPJ1[i]){
+                letrasPalabraJ1[i] = letra;
+                verificarPJ1[i] = true;
+                letraEntrada = true ;
             }
         }
-        if (!letraEntrada){
-            intentos1 = intentos1 - 1;
-
-        }
-        return 
     }
+
+    if(!letraEncontrada){
+        intentos1--;
+        if (intentos1 <= 0){
+            finDelJuego = true;
+            return "cantidad de intentos agotados, has perdido";
+        } else{
+            return "letra incorrecta tienes " + intentos1 + "intentos"; 
+        }
+
+    } else {
+        boollean gano = true;
+        for(boolean b : verificarPJ1) {
+            if (!b){
+                gano = false;
+                break;
+          }
+        }
+        if (gano) {
+            finDelJuego = true;
+            return "Felicidades adivino la palabra"
+        }
+        return "letra correcta"
+    }
+
+
+
+
 
 
 
