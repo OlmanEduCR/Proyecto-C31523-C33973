dd2968bd923132d59cc17bbaeb1eb740ebd9adfa
Cristhian892 <crishc1230@gmail.com>
2025-05-08 23:11:41 -0600

    Actualizacion

--- JuegoAhorcado.java
+++ JuegoAhorcado.java
@@ -13,7 +13,7 @@ public class JuegoAhorcado {
                         "       \n" +
                         "       \n" +
                         "       \n" +
-                        "       \n";
+                        "_______\n";
             case 1:
                 return  "  _____\n" +
                         "  |   |\n" +
@@ -42,7 +42,7 @@ public class JuegoAhorcado {
                 return  "   _____\n" +
                         "   |   |\n" +
                         "   0   |\n" +
-                        "  /|\\  |\n" +  
+                        "  /|\\ |\n" +  
                         "       |\n" +
                         "       |\n" +
                         "_______|\n";
@@ -50,7 +50,7 @@ public class JuegoAhorcado {
                 return  "   _____\n" +
                         "   |   |\n" +
                         "   0   |\n" +
-                        "  /|\\  |\n" +
+                        "  /|\\ |\n" +
                         "  /    |\n" +
                         "       |\n" +
                         "_______|\n";
@@ -58,8 +58,8 @@ public class JuegoAhorcado {
                 return  "   _____\n" +
                         "   |   |\n" +
                         "   0   |\n" +
-                        "  /|\\  |\n" +
-                        "  / \\  |\n" +
+                        "  /|\\ |\n" +
+                        "  / \\ |\n" +
                         "       |\n" +
                         "_______|\n";
             default:
@@ -73,7 +73,7 @@ public class JuegoAhorcado {
         while (true) {
             System.out.println("Jugador " + jugador + " ingrese una palabra de 6 letras ");
             palabra = scanner.nextLine().trim().toLowerCase(); //
-            if(palabra.length() == 6 && palabra.matches("[a-z]+")) {
+            if(palabra.length() == 6 && palabra.matches("[a-zA-Z]+")) {
                 break;
             } else {
                 System.out.println("Palabra incorrecta, debe de tener 6 letras");
@@ -88,10 +88,10 @@ public class JuegoAhorcado {
        
         //solicitar las palabras 
         String palabra1 = solicitarPalabra(1);
-        for(int i = 0; i < 30; i++) System.out.println(); //impreme 30 lienas en blanco
+        for(int i = 0; i < 50; i++) System.out.println(); //impreme 30 lienas en blanco
 
         String palabra2 = solicitarPalabra(2);
-        for(int i = 0; i < 30; i++) System.out.println();
+        for(int i = 0; i < 50; i++) System.out.println();
       
 
 
@@ -104,7 +104,7 @@ public class JuegoAhorcado {
                 System.out.println("Ingresar letra ");
                 String entrada = scanner.nextLine();
                 //si la persona ingresa verias letras puede causar problemas enotnces para evitar eso usamos lo siguiente
-                if(entrada.length() != 1|| !entrada.matches("[a-z]")) {
+                if(entrada.length() != 1|| !entrada.matches("[a-zA-Z]+")) {
                     System.out.println("Ingrese solo una letra");
                     continue;
                 }
@@ -130,7 +130,7 @@ public class JuegoAhorcado {
                 
                 if (juego.getFinDelJuego()){
                     System.out.println("Juego terminado");
-                    System.out.println("Avance final del jugador" + turnoActual + juego.avanceEnLaPalabra(turnoActual));
+                    System.out.println("Avance final del jugador " + turnoActual + juego.avanceEnLaPalabra(turnoActual));
                     break;
                 }
             }
